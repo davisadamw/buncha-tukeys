be7af9d772de6c239a9e146f1e6d49c68767b9a5
Adam Davis <davisadamw@gmail.com>
2020-08-05 17:12:56 -0700

    handle a common warning that doesn't matter for our analysis

--- 99_letters-functions.R
+++ 99_letters-functions.R
@@ -17,11 +17,27 @@ run_multicomp <- function(data, group_var, continuous_var) {
 }
 
 
+
 # function to grab the letters from the multiple comparisons result
 grab_letters_df <- function(glht_obj, group_var, level = 0.05) {
-  mcletters <- glht_obj %>% 
-    multcomp::cld(level = level) %>% 
-    pluck("mcletters")
+  
+  # multcomp::cld generates lots of warnings that can be safely ignored
+  # a quietly function will capture the warnings and let us decide which we want to pass forwards
+  quiet_cld <- quietly(multcomp::cld)
+  
+  cld_result <- glht_obj %>% 
+    quiet_cld(level = level)
+  
+  # check warnings, send along any that don't match our criteria to ignore
+  cld_warnings <- cld_result$warnings
+  if (length(cld_warnings) > 0) {
+    cld_warnings_to_raise <- cld_warnings[!str_starts(cld_warnings, "Completion with")]
+    if (length(cld_warnings_to_raise) > 0) warning(cld_warnings_to_raise)
+  }
+  
+  # if the warnings weren't bad, keep the analysis going
+  mcletters <- cld_result %>% 
+    pluck("result", "mcletters")
   
   # we only really want a couple items out of this thing, 
   # make a data frame out of those including a column for the original treatment group
